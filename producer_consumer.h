775abc99b7069a4ac674eb937e53231aa2df8ae3
hshih <hshih@localhost>
2022-05-04 19:51:45 -0500

    rename

---
+++ producer_consumer.h
@@ -0,0 +1,37 @@
+#ifndef PRODUCER_CONSUMER_H
+#define PRODUCER_CONSUMER_H
+#include <queue>
+#include <mutex>
+
+template<typename T>
+class consumer_queue {
+    public:
+        consumer_queue(){
+            this->max = 4;
+        };
+        
+        bool consumer_ready() {
+            return this->buffer.empty();
+        };
+
+        bool producer_ready() {
+            return this->buffer.size() == this->max;
+        }
+        
+        void do_fill(T data) {
+            this->buffer.push(data);
+        };
+        
+        T do_get() {
+            T res = this->buffer.front();
+            this->buffer.pop();
+            return res;
+        };
+
+    private:
+        std::queue<T> buffer;
+        int max;
+};
+
+
+#endif
