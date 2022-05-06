0dbfa3df425c9834016101c1f018a2fea060b075
hshih <hshih@localhost>
2022-05-06 14:18:25 -0500

    rename consumer_queue

---
+++ consumer_queue.h
@@ -0,0 +1,35 @@
+#ifndef CONSUMER_QUEUE_H
+#define CONSUMER_QUEUE_H
+#include <queue>
+#include <mutex>
+
+template<typename T>
+class consumer_queue {
+    public:
+        consumer_queue();
+        
+        bool consumer_ready() {
+            return this->buffer.empty();
+        };
+
+        bool producer_ready() {
+            return this->buffer.size() == this->max;
+        }
+        
+        void do_fill(T data) {
+            this->buffer.push(data);
+        };
+        
+        T do_get() {
+            T res = this->buffer.front();
+            this->buffer.pop();
+            return res;
+        };
+
+    private:
+        std::queue<T> buffer;
+        int max{4};
+};
+
+
+#endif
