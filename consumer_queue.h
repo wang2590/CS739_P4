036b0f31af1ced62c97b9a1eb3de93578496693a
hshih <hshih@localhost>
2022-05-06 19:39:45 -0500

    format

--- consumer_queue.h
+++ consumer_queue.h
@@ -8,49 +8,49 @@ class consumer_queue {
  public:
   consumer_queue();
 
-  int do_fill(T data) { 
-		std::unique_lock<std::mutex> ul(lock);
-		while (buffer.size() == max) {
-			std::this_thread::sleep_for(std::chrono::microseconds(1));
-		}
+  int do_fill(T data) {
+    std::unique_lock<std::mutex> ul(lock);
+    while (buffer.size() == max) {
+      std::this_thread::sleep_for(std::chrono::microseconds(1));
+    }
 
-		this->buffer.push(data); 
-		ul.unlock();
-		return 0;
-	};
+    this->buffer.push(data);
+    ul.unlock();
+    return 0;
+  };
 
   int do_get(auto start_time, T& res) {
-		while (this->buffer.empty()) {
-			auto end_time = std::chrono::high_resolution_clock::now();
+    while (this->buffer.empty()) {
+      auto end_time = std::chrono::high_resolution_clock::now();
       if (std::chrono::duration_cast<std::chrono::microseconds>(end_time -
                                                                 start_time)
               .count() >= time_out) {
         std::cout << "Timeout: Read Failed!\n";
         return -1;
       }
-			std::this_thread::sleep_for (std::chrono::microseconds(1));
-		}
+      std::this_thread::sleep_for(std::chrono::microseconds(1));
+    }
 
-		auto end_time = std::chrono::high_resolution_clock::now();
+    auto end_time = std::chrono::high_resolution_clock::now();
     if (std::chrono::duration_cast<std::chrono::microseconds>(end_time -
                                                               start_time)
             .count() >= time_out) {
       std::cout << "Timeout: Read Failed!\n";
       return -1;
     }
-    
-		std::unique_lock<std::mutex> ul(q.lock);
+
+    std::unique_lock<std::mutex> ul(q.lock);
     res = this->buffer.front();
     this->buffer.pop();
-		ul.unlock();
+    ul.unlock();
     return 0;
   };
 
  private:
   std::queue<T> buffer;
   int max{4};
-	int time_out{10000};
-	std::mutex lock;
+  int time_out{10000};
+  std::mutex lock;
 };
 
 #endif
