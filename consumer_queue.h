f43b8c960e3b1cbc0c86850cbddcb5f4952543fe
Hao-Yu Shih <hshih25@wisc.edu>
2022-05-10 20:59:08 -0500

    add default in constructor

--- consumer_queue.h
+++ consumer_queue.h
@@ -3,39 +3,31 @@
 #include <mutex>
 #include <queue>
 #include <thread>
-typedef std::chrono::time_point<std::chrono::high_resolution_clock> time_type;
+#include <condition_variable>
+using Clock = std::chrono::system_clock;
+using Duration = std::chrono::duration<long int, std::ratio<1, 1000000000>>;
+using TimePoint = std::chrono::time_point<Clock, Duration>;
 
 
 template <typename T>
 class consumer_queue {
  public:
-  consumer_queue();
+  consumer_queue() = default;
 
   int do_fill(T data) {
     std::unique_lock<std::mutex> ul(lock);
-    while (buffer.size() == max) {
-      std::this_thread::sleep_for(std::chrono::microseconds(1));
-    }
-
     this->buffer.push(data);
     ul.unlock();
     return 0;
   };
 
-  int do_get(time_type start_time, T& res) {
+  int do_get(TimePoint time_out, T& res) {
     std::unique_lock<std::mutex> ul(lock);
     while (this->buffer.empty()) {
-      if (time_checker(start_time)) {
+      if (cv.wait_until(ul, time_out) == std::cv_status::timeout) {
         return -1;
       }
-      std::this_thread::sleep_for(std::chrono::microseconds(1));
     }
-
-    if (!time_checker(start_time)) {
-      return -1;
-    }
-
-    
     res = this->buffer.front();
     this->buffer.pop();
     ul.unlock();
@@ -43,20 +35,9 @@ class consumer_queue {
   };
 
  private:
-  bool time_checker(time_type start_time) {
-    auto end_time = std::chrono::high_resolution_clock::now();
-    if (std::chrono::duration_cast<std::chrono::microseconds>(end_time -
-                                                              start_time)
-            .count() >= time_out) {
-      std::cout << "Timeout: Read Failed!\n";
-      return false;
-    }
-    return true;
-  };
   std::queue<T> buffer;
-  int max{4};
-  int time_out{10000};
   std::mutex lock;
+  std::condition_variable cv;
 };
 
 #endif
