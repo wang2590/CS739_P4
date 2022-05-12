962723cace6cd88010c1f879c50047ac38393ab3
domen111 <domen111@gmail.com>
2022-05-12 15:37:30 -0500

    Add mount_path to config.json

--- common.h
+++ common.h
@@ -1,5 +1,6 @@
+#include <string>
+
 constexpr int kBlockSize = 4 * 1024;
 constexpr char kTempFilesPath[] = "./write_temp_file";
-constexpr char kMountPath[] = "./p4_block";
 
-int get_mount_file();
+int get_mount_file(std::string mount_path);
